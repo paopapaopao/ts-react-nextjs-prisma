008f84dacaf05bc0d41a70852d47728bd602d0eb
paopapaopao <79187858+paopapaopao@users.noreply.github.com>
2024-10-20 14:53:02 +0800

    [Feature] Create Post page (#3)

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,22 +1,36 @@
 import clsx from "clsx";
+import Link from "next/link";
 import { type ReactNode } from "react";
-import { DummyJSONPost } from "@/types";
+import { type DummyJSONPost } from "@/types";
 import styles from "./PostCard.module.css";
 
 interface Props {
   className?: string;
+  isLink?: boolean;
   post: DummyJSONPost | null;
 }
 
-const PostCard = ({ className = "", post }: Props): ReactNode => {
+const PostCard = ({
+  className = "",
+  isLink = false,
+  post,
+}: Props): ReactNode => {
   const classNames: string = clsx(
     "post-card",
     styles["post-card"],
-    "px-8 py-4 flex flex-col gap-4 bg-zinc-800 rounded-lg shadow-lg text-white",
+    isLink && styles["post-card-link"],
+    `px-8 py-4 flex flex-col gap-4 bg-zinc-800 rounded-lg shadow-lg ${
+      isLink && "hover:shadow-xl"
+    } text-white`,
     className
   );
 
-  return (
+  return isLink ? (
+    <Link href={`/posts/${post?.id}`} className={classNames}>
+      <h4 className={clsx("text-lg font-bold", styles.title)}>{post?.title}</h4>
+      <p className="text-base">{post?.body}</p>
+    </Link>
+  ) : (
     <div className={classNames}>
       <h4 className={clsx("text-lg font-bold")}>{post?.title}</h4>
       <p className="text-base">{post?.body}</p>
