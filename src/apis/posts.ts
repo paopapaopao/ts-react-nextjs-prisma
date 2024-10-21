c33a506b81863712fcd8de080e662b51f9e30183
paopapaopao <paolo.casugay@gmail.com>
2024-10-21 10:32:55 +0800

    Create readPost and readPosts apis

---
+++ src/apis/posts.ts
@@ -0,0 +1,28 @@
+import { Prisma, type Post } from '@prisma/client';
+import { prisma } from '@/lib';
+
+const readPost = async (options: Prisma.PostFindUniqueArgs): Promise<Post | null> => {
+  let response: Post | null = null;
+
+  try {
+    response = await prisma.post.findUnique(options);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return response;
+};
+
+const readPosts = async (options: Prisma.PostFindManyArgs): Promise<Post[]> => {
+  let response: Post[] = [];
+
+  try {
+    response = await prisma.post.findMany(options);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return response;
+};
+
+export { readPost, readPosts };
