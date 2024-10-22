b9c9b8be3f3b684ceda56c9cbacbf2cd775fee24
paopapaopao <paolo.casugay@gmail.com>
2024-10-22 10:08:30 +0800

    Add PostForm to Post page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,8 @@
 import clsx from 'clsx';
 import React from 'react';
 import { type Post } from '@prisma/client';
-import { readPost } from '@/apis';
-import { PostCard } from '@/components';
+import { readPost, updatePost } from '@/apis';
+import { PostCard, PostForm } from '@/components';
 
 interface Props {
   params: {
@@ -17,6 +17,21 @@ const Page = async ({ params: { id } }: Props): Promise<JSX.Element> => {
     },
   });
 
+  const updatePostAction = async (formData: FormData): Promise<void> => {
+    'use server';
+
+    const title = formData.get('title');
+    const body = formData.get('body');
+
+    const data = {
+      id: Number(id),
+      title: String(title),
+      body: String(body),
+    };
+
+    await updatePost(data);
+  };
+
   const classNames: string = clsx(
     'post-details-page',
     'p-8 flex flex-col items-center gap-4'
@@ -24,6 +39,10 @@ const Page = async ({ params: { id } }: Props): Promise<JSX.Element> => {
 
   return (
     <main className={classNames}>
+      <PostForm
+        action={updatePostAction}
+        post={post}
+      />
       <h1 className='text-xl font-bold'>Post {id}</h1>
       <PostCard post={post} />
     </main>
