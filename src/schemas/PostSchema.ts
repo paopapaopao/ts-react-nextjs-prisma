47c3df88b71249e5fd720d5332fc83c5014d9c43
paopapaopao <paolo.casugay@gmail.com>
2024-10-24 11:13:59 +0800

    Create PostSchema schema

---
+++ src/schemas/PostSchema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const PostSchema = z.object({
+  id: z.number().int().positive().finite().optional(),
+  body: z
+    .string()
+    .trim()
+    .min(1, { message: 'Must be at least 1 character long' })
+    .max(1000, { message: 'Must be at most 1000 characters long' }),
+  title: z
+    .string()
+    .trim()
+    .min(1, { message: 'Must be at least 1 character long' })
+    .max(100, { message: 'Must be at most 100 characters long' }),
+});
+
+export default PostSchema;
