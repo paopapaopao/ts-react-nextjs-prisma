3c0de08716461941f625459648923584c0d2f156
paopapaopao <paolo.casugay@gmail.com>
2024-10-26 00:12:40 +0800

    Create PUT route for posts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { type SafeParseReturnType } from 'zod';
 import { type Post } from '@prisma/client';
-import { createPost } from '@/actions';
+import { createPost, updatePost } from '@/actions';
 import { postSchema } from '@/lib/schemas';
 import { type PostSchema } from '@/lib/types';
 
@@ -40,4 +40,34 @@ const POST = async (
   });
 };
 
-export { POST };
+const PUT = async (
+  request: NextRequest
+): Promise<
+  NextResponse<{
+    data: Post | null;
+    errors: { [key: string]: string[] } | null;
+    success: boolean;
+  }>
+> => {
+  const payload: unknown = await request.json();
+  const parsedPayload: SafeParseReturnType<PostSchema, PostSchema> =
+    postSchema.safeParse(payload);
+
+  if (!parsedPayload.success) {
+    return NextResponse.json({
+      data: null,
+      errors: parsedPayload.error?.flatten().fieldErrors,
+      success: false,
+    });
+  }
+
+  const post: Post | null = await updatePost(parsedPayload.data);
+
+  return NextResponse.json({
+    data: post,
+    errors: null,
+    success: true,
+  });
+};
+
+export { POST, PUT };
