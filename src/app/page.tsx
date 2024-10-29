dd13f9cf5d9f8ae15fc1d7c69b9a38b7016945bc
paopapaopao <paolo.casugay@gmail.com>
2024-10-29 20:43:02 +0800

    Add infinite scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import clsx from 'clsx';
-import { type Post } from '@prisma/client';
-import { readPosts } from '@/actions';
-import { PostCard, PostForm } from '@/components';
+import { type ReactNode } from 'react';
+import { PostForm, PostList } from '@/components';
 
-const Page = async (): Promise<JSX.Element> => {
-  const posts: Post[] = await readPosts({
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
-
+const Page = (): ReactNode => {
   const classNames: string = clsx(
     'home-page',
     'p-8 flex flex-col items-center gap-4'
@@ -18,16 +11,7 @@ const Page = async (): Promise<JSX.Element> => {
   return (
     <main className={classNames}>
       <PostForm />
-      <ul className='p-8 flex flex-col items-center gap-4'>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <PostCard
-              post={post}
-              isLink
-            />
-          </li>
-        ))}
-      </ul>
+      <PostList />
     </main>
   );
 };
