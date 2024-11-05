9a8d72bdfec98d2b1e41b31ef93fa1db19fd85b3
paopapaopao <paolo.casugay@gmail.com>
2024-11-05 19:50:31 +0800

    Create PostCardSkeleton component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { Button } from './Button';
 export { Modal } from './Modal';
 export { PostCard } from './PostCard';
+export { PostCardSkeleton } from './PostCardSkeleton';
 export { PostForm } from './PostForm';
 export { PostList } from './PostList';
 export { SearchPostField } from './SearchPostField';
