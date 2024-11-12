eb4c095f397cb2cce19e7e606a759b695ae82c53
paopapaopao <paolo.casugay@gmail.com>
2024-11-12 22:20:28 +0800

    Create post mock data

---
+++ src/test/mock-data/posts.ts
@@ -0,0 +1,7 @@
+const post = {
+  id: 1,
+  body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+  title: 'His mother had always taught him',
+};
+
+export { post };
