0a8b062615e5430fbd12fc5d3704aa199243bc48
paopapaopao <paolo.casugay@gmail.com>
2024-11-21 15:29:55 +0800

    Add responsive styles for PostCardSkeleton

--- src/components/PostCardSkeleton/PostCardSkeleton.tsx
+++ src/components/PostCardSkeleton/PostCardSkeleton.tsx
@@ -1,21 +1,22 @@
 import clsx from 'clsx';
 import { type ReactNode } from 'react';
 import { Skeleton } from '@nextui-org/react';
-import styles from './PostCardSkeleton.module.css';
 
 const PostCardSkeleton = (): ReactNode => {
   const classNames: string = clsx(
-    styles['post-card-skeleton'],
-    `px-8 py-4 flex flex-col gap-4 bg-zinc-800 rounded-lg shadow-lg`
+    'px-4 py-2 min-w-[328px] w-full max-w-screen-xl flex flex-col gap-2',
+    'md:px-6 md:py-3 md:gap-3',
+    'xl:px-8 xl:py-4 xl:gap-4',
+    'rounded-lg bg-zinc-800'
   );
 
   return (
     <div className={classNames}>
-      <Skeleton className='w-[50%] h-6 bg-zinc-600 rounded-lg' />
+      <Skeleton className='w-[50%] h-6 rounded-lg bg-zinc-600' />
       <div className='flex flex-col gap-2'>
-        <Skeleton className='w-[90%] h-4 bg-zinc-600 rounded-lg' />
-        <Skeleton className='w-[100%] h-4 bg-zinc-600 rounded-lg' />
-        <Skeleton className='w-[20%] h-4 bg-zinc-600 rounded-lg' />
+        <Skeleton className='w-[90%] h-4 rounded-lg bg-zinc-600' />
+        <Skeleton className='w-[100%] h-4 rounded-lg bg-zinc-600' />
+        <Skeleton className='w-[20%] h-4 rounded-lg bg-zinc-600' />
       </div>
     </div>
   );
