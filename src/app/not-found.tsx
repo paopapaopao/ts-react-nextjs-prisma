286ec7ef69e48570f3fb8447f95ec3ccbf8c0565
paopapaopao <paolo.casugay@gmail.com>
2024-11-22 18:08:07 +0800

    Add responsive styles for NotFound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,15 +13,16 @@ const NotFound = (): ReactNode => {
   };
 
   const classNames: string = clsx(
-    'not-found-page',
-    'p-8 flex flex-col items-center gap-8'
+    'p-2 flex flex-col justify-center items-center gap-4',
+    'md:p-5 md:gap-6',
+    'xl:p-8 xl:gap-8'
   );
 
   return (
     <main className={classNames}>
       <h1 className='text-xl font-bold text-red-600'>Not Found</h1>
       <p className='text-lg'>Could not find requested resource</p>
-      <div className='flex gap-4'>
+      <div className='mb-[65px] flex gap-4'>
         <Button onClick={back}>Return to previous page</Button>
         <Button onClick={handleClick}>Go to Home page</Button>
       </div>
