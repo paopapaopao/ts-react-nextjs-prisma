fc8ca229b2d13eeafdb513f57e8e0f877d31564d
paopapaopao <paolo.casugay@gmail.com>
2024-11-22 18:24:31 +0800

    Add responsive styles for Error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -18,18 +18,19 @@ const Error = ({ error, reset }: Props): ReactNode => {
   };
 
   const classNames: string = clsx(
-    'error-page',
-    'p-8 flex flex-col items-center gap-8'
+    'p-2 flex flex-col justify-center items-center gap-4',
+    'md:p-5 md:gap-6',
+    'xl:p-8 xl:gap-8'
   );
 
   return (
     <main className={classNames}>
       <h1 className='text-xl font-bold text-red-600'>Error</h1>
-      <p className='text-lg'>
+      <p className='text-center text-lg'>
         {error.name}: {error.message}
       </p>
       <Button onClick={reset}>Try again</Button>
-      <div className='flex gap-4'>
+      <div className='mb-[65px] flex gap-4'>
         <Button onClick={back}>Return to previous page</Button>
         <Button onClick={handleClick}>Go to Home page</Button>
       </div>
