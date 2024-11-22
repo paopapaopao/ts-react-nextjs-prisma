4fdc9893d0c868827b3fe7486d1f3a99984c18b5
paopapaopao <paolo.casugay@gmail.com>
2024-11-22 17:24:36 +0800

    Add responsive styles for Sign up page

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,16 @@
+import clsx from 'clsx';
 import { type ReactNode } from 'react';
 import { SignUp } from '@clerk/nextjs';
 
 const Page = (): ReactNode => {
+  const classNames: string = clsx(
+    'p-2 flex justify-center items-center',
+    'md:p-5',
+    'xl:p-8'
+  );
+
   return (
-    <main className='p-8 flex justify-center items-center'>
+    <main className={classNames}>
       <SignUp />
     </main>
   );
