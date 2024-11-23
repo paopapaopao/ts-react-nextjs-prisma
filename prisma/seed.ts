956211f82244629d80f4daaa339cc8f007cbe219
paopapaopao <paolo.casugay@gmail.com>
2024-11-23 15:14:48 +0800

    Add comments seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,28 @@
 import { PrismaClient } from '@prisma/client';
-import { type DummyJSONPost } from '@/lib/types';
+import { type DummyJSONComment, type DummyJSONPost } from '@/lib/types';
 
 const prisma = new PrismaClient();
+
+const getComments = async (): Promise<DummyJSONComment[]> => {
+  let comments: DummyJSONComment[] = [];
+
+  try {
+    const response: Response = await fetch(
+      'https://dummyjson.com/comments?limit=0&select=id,body,postId'
+    );
+
+    if (!response.ok) {
+      throw new Error('An error occurred while getting comments.');
+    }
+
+    const data: { comments: DummyJSONComment[] } = await response.json();
+    comments = data.comments;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return comments;
+};
 
 const getPosts = async (): Promise<DummyJSONPost[]> => {
   let posts: DummyJSONPost[] = [];
@@ -25,17 +46,32 @@ const getPosts = async (): Promise<DummyJSONPost[]> => {
 };
 
 async function main() {
+  await prisma.comment.deleteMany({});
+  // *Resets the id to 1
+  await prisma.$executeRawUnsafe(
+    `ALTER SEQUENCE "Comment_id_seq" RESTART WITH 1`
+  );
   await prisma.post.deleteMany({});
   // *Resets the id to 1
   await prisma.$executeRawUnsafe(`ALTER SEQUENCE "Post_id_seq" RESTART WITH 1`);
 
   const initialPosts: DummyJSONPost[] = await getPosts();
+  const initialComments: DummyJSONComment[] = await getComments();
 
   for (const post of initialPosts) {
     await prisma.post.create({
       data: {
         body: post.body,
         title: post.title,
+      },
+    });
+  }
+
+  for (const comment of initialComments) {
+    await prisma.comment.create({
+      data: {
+        body: comment.body,
+        postId: comment.postId,
       },
     });
   }
