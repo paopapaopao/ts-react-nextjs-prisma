67a064ab8788f74c7bce526d353d4770785454fd
paopapaopao <paolo.casugay@gmail.com>
2024-11-25 16:31:21 +0800

    Create PostCardLink component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { Button } from './Button';
 export { Modal } from './Modal';
 export { PostCard } from './PostCard';
+export { PostCardLink } from './PostCardLink';
 export { PostCardSkeleton } from './PostCardSkeleton';
 export { PostForm } from './PostForm';
 export { PostList } from './PostList';
