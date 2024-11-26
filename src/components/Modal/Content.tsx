92ae3d843286e3bd544534dcf6392b27961b095d
paopapaopao <paolo.casugay@gmail.com>
2024-11-26 20:46:50 +0800

    Update parallel intercepted post modal

--- src/components/Modal/Content.tsx
+++ src/components/Modal/Content.tsx
@@ -3,10 +3,11 @@ import { type ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+  className: string;
 }
 
-const Content = ({ children }: Props): ReactNode => {
-  const classNames: string = clsx('py-2', 'md:py-3', 'xl:py-4');
+const Content = ({ children, className }: Props): ReactNode => {
+  const classNames: string = clsx('py-2', 'md:py-3', 'xl:py-4', className);
 
   return <div className={classNames}>{children}</div>;
 };
