0f14a63887fceb0970a4cb99bbc863879349c7d1
paopapaopao <paolo.casugay@gmail.com>
2024-11-28 23:11:28 +0800

    Update PostForm component

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -9,10 +9,14 @@ import { type PostSchema } from '@/lib/types';
 import { Button } from '../Button';
 
 interface Props {
+  className?: string;
   post?: PostSchema | null;
 }
 
-const PostForm = ({ post }: Props): ReactNode => {
+// *NOTE: Temporary
+const USER_ID = 21;
+
+const PostForm = ({ className = '', post }: Props): ReactNode => {
   const {
     formState: { errors, isSubmitting },
     handleSubmit,
@@ -22,32 +26,35 @@ const PostForm = ({ post }: Props): ReactNode => {
     resolver: zodResolver(postSchema),
     defaultValues: {
       id: post?.id,
-      title: post?.title,
-      body: post?.body,
+      title: post?.title || '',
+      body: post?.body || '',
+      userId: post?.userId || USER_ID,
     },
   });
 
-  const onSubmit = async (data: PostSchema): Promise<void> => {
+  const onSubmit = async (formData: PostSchema): Promise<void> => {
+    const data = {
+      id: formData.id,
+      title: formData.title,
+      body: formData.body,
+      userId: formData.userId,
+    };
+
     await fetch(`/api/${post ? `posts/${post.id}` : 'posts'}`, {
       method: post ? 'PUT' : 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: data.id,
-        title: data.title,
-        body: data.body,
-      }),
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
     });
 
     reset();
   };
 
   const classNames: string = clsx(
-    'p-4 min-w-[344px] w-full max-w-screen-md flex flex-col gap-4',
-    'md:p-6 md:gap-6',
-    'xl:p-8 xl:gap-8',
-    'rounded-lg bg-white'
+    'min-w-[344px] w-full flex flex-col gap-4',
+    'md:gap-6',
+    'xl:gap-8',
+    'bg-zinc-800',
+    className
   );
 
   const buttonText = post ? 'Update post' : 'Create post';
@@ -64,28 +71,33 @@ const PostForm = ({ post }: Props): ReactNode => {
           className='hidden'
         />
       )}
-      <label className='flex flex-col gap-2 text-sm font-bold text-gray-700'>
+      <label className='flex flex-col gap-2 text-sm font-bold text-white'>
         Title
         <input
           {...register('title')}
           name='title'
           type='text'
           placeholder='Enter title'
-          className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+          className='bg-zinc-700 shadow border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
         />
         {errors.title && <p>{`${errors.title.message}`}</p>}
       </label>
-      <label className='flex flex-col gap-2 text-sm font-bold text-gray-700'>
+      <label className='flex flex-col gap-2 text-sm font-bold text-white'>
         Body
         <textarea
           {...register('body')}
           name='body'
           rows={4}
           placeholder='Enter body'
-          className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none'
+          className='bg-zinc-700 shadow border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline resize-none'
         />
         {errors.body && <p>{`${errors.body.message}`}</p>}
       </label>
+      <input
+        {...register('userId')}
+        name='userId'
+        className='hidden'
+      />
       <Button disabled={isSubmitting}>{buttonText}</Button>
     </form>
   );
