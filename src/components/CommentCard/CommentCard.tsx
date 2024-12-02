813be797cadd747a33bbab7e604a571e1cc664cb
paopapaopao <paolo.casugay@gmail.com>
2024-12-02 20:36:33 +0800

    Create CommentCardContext

--- src/components/CommentCard/CommentCard.tsx
+++ src/components/CommentCard/CommentCard.tsx
@@ -2,11 +2,11 @@
 
 import Image from 'next/image';
 import { type ReactNode, useState } from 'react';
-import { FaRegEdit } from 'react-icons/fa';
-import { RiDeleteBin6Line } from 'react-icons/ri';
 import defaultProfilePhoto from '@/assets/images/default-profile-photo.jpg';
-import { CommentWithUser } from '@/lib/types';
+import { type CommentWithUser } from '@/lib/types';
 import { CommentForm } from '../CommentForm';
+import CommentCardActions from './CommentCardActions';
+import CommentCardContext from './CommentCardContext';
 
 interface Props {
   comment: CommentWithUser;
@@ -19,45 +19,32 @@ const CommentCard = ({ comment }: Props): ReactNode => {
     setMode((mode: 'VIEW' | 'EDIT') => (mode === 'VIEW' ? 'EDIT' : 'VIEW'));
   };
 
-  const handleDeleteClick = async (): Promise<void> => {
-    await fetch(`/api/comments/${comment?.id}`, { method: 'DELETE' });
-  };
-
   const fullName: string = `${comment?.user.firstName} ${comment?.user.lastName}`;
 
   return (
-    <div className='flex gap-2'>
-      <Image
-        src={comment?.user.image || defaultProfilePhoto}
-        width={48}
-        height={48}
-        alt='Default profile photo'
-        className='rounded-full'
-      />
-      {mode === 'VIEW' ? (
-        <div className='flex-auto flex flex-col gap-2'>
-          <span className='text-sm'>{fullName}</span>
-          <p className='flex-auto'>{comment?.body}</p>
-        </div>
-      ) : (
-        <CommentForm
-          comment={comment}
-          className='flex-auto'
+    <CommentCardContext.Provider value={{ comment }}>
+      <div className='flex gap-2'>
+        <Image
+          src={comment?.user.image || defaultProfilePhoto}
+          width={48}
+          height={48}
+          alt='Default profile photo'
+          className='rounded-full'
         />
-      )}
-      <button onClick={handleModeToggle}>
-        <FaRegEdit
-          className='self-center'
-          size={16}
-        />
-      </button>
-      <button onClick={handleDeleteClick}>
-        <RiDeleteBin6Line
-          className='self-center'
-          size={16}
-        />
-      </button>
-    </div>
+        {mode === 'VIEW' ? (
+          <div className='flex-auto flex flex-col gap-2'>
+            <span className='text-sm'>{fullName}</span>
+            <p className='flex-auto'>{comment?.body}</p>
+          </div>
+        ) : (
+          <CommentForm
+            comment={comment}
+            className='flex-auto'
+          />
+        )}
+        <CommentCardActions onToggle={handleModeToggle} />
+      </div>
+    </CommentCardContext.Provider>
   );
 };
 
