85c1ab78164a76bbb9cbcea180fc2a2c2d5caad2
paopapaopao <paolo.casugay@gmail.com>
2024-12-03 15:40:23 +0800

    Add clerkUserId to schemas

--- src/lib/schemas/commentSchema.ts
+++ src/lib/schemas/commentSchema.ts
@@ -9,6 +9,7 @@ const commentShape = {
     .max(1000, { message: 'Must be at most 1000 characters long' }),
   postId: z.number().int().positive().finite(),
   userId: z.number().int().positive().finite(),
+  clerkUserId: z.string().optional(),
 };
 
 const commentSchema = z.object(commentShape);
