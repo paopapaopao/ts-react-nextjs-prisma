9ab4cc9a9c072044dfe43f184d89d7135ec14894
paopapaopao <paolo.casugay@gmail.com>
2024-12-17 23:56:02 +0800

    Refactor

--- src/lib/utils/getFullName.ts
+++ src/lib/utils/getFullName.ts
@@ -1,7 +1,7 @@
 import { type User } from '@prisma/client';
 
-const getFullName = (user: User): string => {
-  return user.firstName + user.lastName;
+const getFullName = (user: User | undefined): string => {
+  return `${user?.firstName} + ${user?.lastName}`;
 };
 
 export default getFullName;
