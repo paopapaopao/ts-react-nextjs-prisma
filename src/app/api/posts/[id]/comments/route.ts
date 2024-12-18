4bba6b5e0362490392283bba1100c8f290aac5a9
paopapaopao <paolo.casugay@gmail.com>
2024-12-18 18:49:04 +0800

    Update GET route for post comments

--- src/app/api/posts/[id]/comments/route.ts
+++ src/app/api/posts/[id]/comments/route.ts
@@ -5,23 +5,41 @@ import { readComments } from '@/lib/actions';
 type GetParams = { params: Promise<{ id: string }> };
 
 type GetReturn = {
-  data: { comments: Comment[] | null };
+  data: {
+    comments: Comment[];
+    nextCursor: number | null;
+  };
   errors: { [key: string]: string[] } | null;
   success: boolean;
 };
 
 const GET = async (
-  _: NextRequest,
+  request: NextRequest,
   { params }: GetParams
 ): Promise<NextResponse<GetReturn>> => {
+  const { searchParams } = new URL(request.url);
+  const cursor: number = Number(searchParams.get('cursor'));
+
   const id: string = (await params).id;
+
   const comments: Comment[] = await readComments({
+    ...(cursor > 0 && {
+      cursor: { id: cursor },
+      skip: 1,
+    }),
     where: { postId: Number(id) },
     include: { user: true },
+    take: 4,
+    orderBy: [{ createdAt: 'asc' }],
   });
 
+  const hasMore: boolean = comments.length > 0;
+
   return NextResponse.json({
-    data: { comments },
+    data: {
+      comments,
+      nextCursor: hasMore ? comments[comments.length - 1].id : null,
+    },
     errors: null,
     success: true,
   });
