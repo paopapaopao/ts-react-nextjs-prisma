26355fe4c41e643e32ac811b9efd9308c4507b30
paopapaopao <paolo.casugay@gmail.com>
2024-12-25 10:48:14 +0800

    Create GET route for /posts/[id]/comments/[parentCommentId]/replies

---
+++ src/app/api/posts/[id]/comments/[parentCommentId]/replies/route.tsx
@@ -0,0 +1,37 @@
+import { type NextRequest, NextResponse } from 'next/server';
+import { type Comment } from '@prisma/client';
+import { readComments } from '@/lib/actions';
+
+type GETParams = {
+  params: Promise<{
+    id: string;
+    parentCommentId: string;
+  }>;
+};
+
+const GET = async (_: NextRequest, { params }: GETParams) => {
+  const id: string = (await params).id;
+  const parentCommentId: string = (await params).parentCommentId;
+
+  const comments: Comment[] = await readComments({
+    where: {
+      postId: Number(id),
+      parentCommentId: Number(parentCommentId),
+    },
+    include: {
+      user: true,
+      _count: {
+        select: { replies: true },
+      },
+    },
+    orderBy: [{ createdAt: 'asc' }],
+  });
+
+  return NextResponse.json({
+    data: { comments },
+    errors: null,
+    success: true,
+  });
+};
+
+export { GET };
