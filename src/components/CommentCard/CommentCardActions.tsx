7ffd2610716dbe8dc14e18decc5fe3feb2067b9d
paopapaopao <79187858+paopapaopao@users.noreply.github.com>
2024-12-28 20:38:26 +0800

    [Feature] Add confirmation dialog on Comment/reply delete (#50)

--- src/components/CommentCard/CommentCardActions.tsx
+++ src/components/CommentCard/CommentCardActions.tsx
@@ -5,8 +5,11 @@ import { type ReactNode } from 'react';
 import { FaRegEdit } from 'react-icons/fa';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { toast } from 'react-toastify';
+import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react';
 
 import { useDeleteComment } from '@/lib/hooks';
+
+import { Button } from '../Button';
 
 import useCommentCard from './useCommentCard';
 
@@ -15,17 +18,18 @@ const CommentCardActions = (): ReactNode => {
 
   const { mutate: deleteComment } = useDeleteComment();
 
+  const type: string = comment?.parentCommentId === null ? 'Comment' : 'Reply';
+
   const handleClick = (): void => {
     deleteComment(comment?.id, {
       onSuccess: (): void => {
-        toast.success(
-          `${
-            comment?.parentCommentId === null ? 'Comment' : 'Reply'
-          } deleted successfully!`
-        );
+        toast.success(`${type} deleted successfully!`);
       },
     });
   };
+
+  const hasReplies: boolean | null =
+    comment && comment._count && comment._count.replies > 0;
 
   const classNames: string = clsx('flex gap-2', 'md:gap-3', 'xl:gap-4');
 
@@ -34,9 +38,31 @@ const CommentCardActions = (): ReactNode => {
       <button onClick={onModeToggle}>
         <FaRegEdit size={16} />
       </button>
-      <button onClick={handleClick}>
-        <RiDeleteBin6Line size={16} />
-      </button>
+      <Popover
+        placement='right'
+        className='text-black'
+      >
+        <PopoverTrigger>
+          <button>
+            <RiDeleteBin6Line size={16} />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent className='gap-2'>
+          <h2 className='text-lg font-bold'>
+            Delete {type.toLocaleLowerCase()}
+          </h2>
+          <p className='text-center'>
+            Are you sure you want to delete this {type.toLocaleLowerCase()}?
+            {hasReplies && (
+              <>
+                <br />
+                All {comment?._count.replies} replies will be deleted as well.
+              </>
+            )}
+          </p>
+          <Button onClick={handleClick}>Delete</Button>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
