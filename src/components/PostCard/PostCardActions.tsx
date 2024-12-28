6460b40b618d5f695c7e6ce488a6fd7de0e144f4
paopapaopao <paolo.casugay@gmail.com>
2024-12-28 21:28:07 +0800

    Add confirmation dialog on delete

--- src/components/PostCard/PostCardActions.tsx
+++ src/components/PostCard/PostCardActions.tsx
@@ -5,8 +5,11 @@ import { type ReactNode } from 'react';
 import { FaRegEdit } from 'react-icons/fa';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { toast } from 'react-toastify';
+import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react';
 
 import { useDeletePost } from '@/lib/hooks';
+
+import { Button } from '../Button';
 
 import usePostCard from './usePostCard';
 
@@ -23,6 +26,9 @@ const PostCardActions = (): ReactNode => {
     });
   };
 
+  const hasComments: boolean | undefined =
+    post && post._count && post._count.comments > 0;
+
   const classNames: string = clsx('flex gap-2', 'md:gap-3', 'xl:gap-4');
 
   return (
@@ -30,9 +36,29 @@ const PostCardActions = (): ReactNode => {
       <button onClick={onModeToggle}>
         <FaRegEdit size={16} />
       </button>
-      <button onClick={handleClick}>
-        <RiDeleteBin6Line size={16} />
-      </button>
+      <Popover
+        placement='right'
+        className='text-black'
+      >
+        <PopoverTrigger>
+          <button>
+            <RiDeleteBin6Line size={16} />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent className='gap-2'>
+          <h2 className='text-lg font-bold'>Delete Post</h2>
+          <p className='text-center'>
+            Are you sure you want to delete this post?
+            {hasComments && (
+              <>
+                <br />
+                All {post?._count?.comments} comments will be deleted as well.
+              </>
+            )}
+          </p>
+          <Button onClick={handleClick}>Delete</Button>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
