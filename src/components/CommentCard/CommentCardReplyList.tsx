09cdb52d4f4ae952e47ee17f989664e769290c2b
paopapaopao <paolo.casugay@gmail.com>
2025-01-02 14:29:27 +0800

    Add infinite scroll for comment replies

--- src/components/CommentCard/CommentCardReplyList.tsx
+++ src/components/CommentCard/CommentCardReplyList.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import clsx from 'clsx';
-import { type ReactNode } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { type ReactNode, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
+import { REPLIES_FETCH_COUNT } from '@/lib/constants';
 import { type CommentWithUserAndReplyCount } from '@/lib/types';
 
 import { CommentCardSkeleton } from '../CommentCardSkeleton';
@@ -15,20 +17,37 @@ const CommentCardReplyList = (): ReactNode => {
   const { comment } = useCommentCard();
 
   // TODO
-  const getCommentReplies = async () => {
-    const response: Response = await fetch(
-      `/api/posts/${comment?.postId}/comments/${comment?.id}/replies`
+  const getReplies = async ({ pageParam }: { pageParam: number }) => {
+    const response = await fetch(
+      `/api/posts/${comment?.postId}/comments/${comment?.id}/replies?cursor=${pageParam}`
     );
 
-    const data = await response.json();
+    if (!response.ok) throw new Error('Network response was not ok');
 
-    return data;
+    return response.json();
   };
 
-  const { data, isLoading } = useQuery({
-    queryFn: getCommentReplies,
+  const {
+    data,
+    error,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryFn: getReplies,
     queryKey: ['replies', comment?.postId, comment?.id],
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.data.nextCursor,
   });
+
+  const { inView, ref } = useInView();
+
+  useEffect((): void => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
 
   const classNames: string = clsx(
     'ms-12 flex flex-col gap-2',
@@ -36,22 +55,56 @@ const CommentCardReplyList = (): ReactNode => {
     'xl:ms-14 xl:gap-4'
   );
 
-  return isLoading ? (
-    <ul className={classNames}>
-      {Array.from({ length: 2 }).map((_: unknown, index: number) => (
-        <li key={`reply-skeleton-${index}`}>
-          <CommentCardSkeleton />
-        </li>
-      ))}
+  return status === 'pending' ? (
+    <ul className={clsx('self-stretch', classNames)}>
+      {Array.from({ length: REPLIES_FETCH_COUNT }).map(
+        (_: unknown, index: number) => (
+          <li
+            key={`reply-skeleton-${index}`}
+            className='self-stretch'
+          >
+            <CommentCardSkeleton />
+          </li>
+        )
+      )}
     </ul>
+  ) : status === 'error' ? (
+    <div>{error.message}</div>
   ) : (
-    <ul className={classNames}>
-      {data?.data?.comments.map((comment: CommentWithUserAndReplyCount) => (
-        <li key={`reply-${comment?.id}`}>
-          <CommentCard comment={comment} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={classNames}>
+        {data.pages.map((page, index: number) => (
+          <li
+            key={`reply-group-${index}`}
+            className='self-stretch'
+          >
+            <ul className={clsx('flex flex-col gap-2', 'md:gap-3', 'xl:gap-4')}>
+              {page.data.comments.map(
+                (comment: CommentWithUserAndReplyCount) => (
+                  <li
+                    key={`reply-${comment?.id}`}
+                    className='self-stretch'
+                  >
+                    <CommentCard comment={comment} />
+                  </li>
+                )
+              )}
+            </ul>
+          </li>
+        ))}
+      </ul>
+      {!hasNextPage && (
+        <div className={clsx('ms-12', 'md:ms-[52px]', 'xl:ms-14')}>
+          All replies loaded.
+        </div>
+      )}
+      <div
+        ref={ref}
+        className='self-stretch'
+      >
+        {isFetchingNextPage && <CommentCardSkeleton />}
+      </div>
+    </>
   );
 };
 
