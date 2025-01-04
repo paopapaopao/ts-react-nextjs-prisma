15351ccea581d7d64256fd1d0af7da7f4c3a060f
paopapaopao <paolo.casugay@gmail.com>
2025-01-04 22:34:23 +0800

    Use ReactionButtonGroup

--- src/components/CommentCard/CommentCardInteractions.tsx
+++ src/components/CommentCard/CommentCardInteractions.tsx
@@ -3,6 +3,8 @@
 import clsx from 'clsx';
 import { type ReactNode } from 'react';
 
+import { ReactionButtonGroup } from '../ReactionButtonGroup';
+
 import useCommentCard from './useCommentCard';
 
 const CommentCardInteractions = (): ReactNode => {
@@ -15,17 +17,23 @@ const CommentCardInteractions = (): ReactNode => {
 
   return (
     <div className={classNames}>
-      <span
-        onClick={onReplyFormToggle}
-        className={clsx(
-          'ms-12',
-          'md:ms-[52px]',
-          'xl:ms-14',
-          'text-xs cursor-pointer'
-        )}
-      >
-        Reply
-      </span>
+      <div className='flex gap-4'>
+        <span
+          onClick={onReplyFormToggle}
+          className={clsx(
+            'ms-12',
+            'md:ms-[52px]',
+            'xl:ms-14',
+            'text-xs cursor-pointer'
+          )}
+        >
+          Reply
+        </span>
+        <ReactionButtonGroup commentId={comment?.id}>
+          <span className='text-xs cursor-pointer'>Like</span>
+          <span className='text-xs cursor-pointer'>Dislike</span>
+        </ReactionButtonGroup>
+      </div>
       {hasReplies && (
         <span
           onClick={onReplyListToggle}
