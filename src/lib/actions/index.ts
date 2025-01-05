eeccbd66244890c03a29c47e3e0004a7ba28acef
paopapaopao <paolo.casugay@gmail.com>
2025-01-05 17:18:52 +0800

    Delete readPosts action

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,6 +1,5 @@
 export { default as readUser } from './readUser';
 
-export { default as readPosts } from './readPosts';
 export { default as readPostWithUserAndCommentsCountAndReactionsCountsAndUserReaction } from './readPostWithUserAndCommentsCountAndReactionsCountsAndUserReaction';
 
 export { default as readComments } from './readComments';
