90955e8a10848ac831afb47fec43d6de813e2985
paopapaopao <paolo.casugay@gmail.com>
2025-01-06 14:10:50 +0800

    Update GET route for /api/posts/[id]/comments

--- src/app/api/posts/[id]/comments/route.ts
+++ src/app/api/posts/[id]/comments/route.ts
@@ -1,8 +1,9 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs/server';
 import { type Comment } from '@prisma/client';
 
-import { readComments } from '@/lib/actions';
 import { COMMENTS_FETCH_COUNT } from '@/lib/constants';
+import { prisma } from '@/lib/db';
 
 type GETParams = {
   params: Promise<{ id: string }>;
@@ -25,7 +26,9 @@ const GET = async (
   const cursor: number = Number(searchParams.get('cursor'));
   const id: number = Number((await params).id);
 
-  const comments: Comment[] = await readComments({
+  const { userId } = await auth();
+
+  const comments = await prisma.comment.findMany({
     ...(cursor > 0 && {
       cursor: { id: cursor },
       skip: 1,
@@ -39,17 +42,59 @@ const GET = async (
       _count: {
         select: { replies: true },
       },
+      reactions: {
+        where: { clerkUserId: userId },
+        select: { type: true },
+      },
     },
     take: COMMENTS_FETCH_COUNT,
     orderBy: { createdAt: 'asc' },
   });
 
-  const hasMore: boolean = comments.length > 0;
+  // TODO
+  const reactionCounts = await prisma.reaction.groupBy({
+    by: ['commentId', 'type'],
+    _count: { type: true },
+  });
+
+  // TODO
+  const commentsWithReactionCounts = comments.map((comment) => {
+    const reactionCount = reactionCounts.reduce(
+      (accumulator, reaction) => {
+        if (reaction.commentId === comment.id) {
+          accumulator[reaction.type] = reaction._count.type;
+        }
+
+        return accumulator;
+      },
+      { LIKE: 0, DISLIKE: 0 }
+    );
+
+    return {
+      ...comment,
+      reactionCount,
+    };
+  });
+
+  // TODO
+  const commentsWithUserReaction = commentsWithReactionCounts.map((comment) => {
+    const userReaction =
+      comment?.reactions?.length > 0 ? comment?.reactions?.[0].type : null;
+
+    return {
+      ...comment,
+      userReaction,
+    };
+  });
+
+  const hasMore: boolean = commentsWithUserReaction.length > 0;
 
   return NextResponse.json({
     data: {
-      comments,
-      nextCursor: hasMore ? comments[comments.length - 1].id : null,
+      comments: commentsWithUserReaction,
+      nextCursor: hasMore
+        ? commentsWithUserReaction[commentsWithUserReaction.length - 1].id
+        : null,
     },
     errors: null,
     success: true,
