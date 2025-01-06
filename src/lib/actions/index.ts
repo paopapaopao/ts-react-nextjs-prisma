9fb1751253bfaf8fd492c6e68e750a42521547d9
paopapaopao <paolo.casugay@gmail.com>
2025-01-06 17:42:52 +0800

    Delete readComments action

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,6 +1,3 @@
-export { default as readUser } from './readUser';
-
-export { default as readPostWithUserAndCommentsCountAndReactionsCountsAndUserReaction } from './readPostWithUserAndCommentsCountAndReactionsCountsAndUserReaction';
-
-export { default as readComments } from './readComments';
 export { default as readCommentWithUserAndRepliesCountAndReactionsCountsAndUserReaction } from './readCommentWithUserAndRepliesCountAndReactionsCountsAndUserReaction';
+export { default as readPostWithUserAndCommentsCountAndReactionsCountsAndUserReaction } from './readPostWithUserAndCommentsCountAndReactionsCountsAndUserReaction';
+export { default as readUser } from './readUser';
