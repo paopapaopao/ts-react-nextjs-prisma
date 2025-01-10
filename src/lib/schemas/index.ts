b4010e3008bed49f98c93682f3b78693aa9d9737
paopapaopao <paolo.casugay@gmail.com>
2025-01-10 08:52:13 +0800

    Create viewSchema schema

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -1,3 +1,4 @@
 export { default as commentSchema } from './commentSchema';
 export { default as postSchema } from './postSchema';
 export { default as reactionSchema } from './reactionSchema';
+export { default as viewSchema } from './viewSchema';
