13226227dc62dc9a40e67343496d1b0546fe4d39
paopapaopao <paolo.casugay@gmail.com>
2025-01-10 09:50:59 +0800

    Create view on page visit

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import clsx from 'clsx';
-import { type ReactNode } from 'react';
+import { type ReactNode, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 import { PostCard, PostCardSkeleton } from '@/components';
+import { useCreateView, useSignedInUser } from '@/lib/hooks';
 
 type Props = {
   params: { id: string };
@@ -23,6 +24,19 @@ const Page = ({ params: { id } }: Props): ReactNode => {
     queryKey: ['post', id],
     queryFn: getPost,
   });
+
+  const { signedInUser } = useSignedInUser();
+
+  const { mutate: createView } = useCreateView();
+
+  useEffect((): void => {
+    if (signedInUser && signedInUser.id && id) {
+      createView({
+        userId: signedInUser.id,
+        postId: Number(id),
+      });
+    }
+  }, [signedInUser, id, createView]);
 
   const classNames: string = clsx(
     'p-2 flex flex-col items-center',
