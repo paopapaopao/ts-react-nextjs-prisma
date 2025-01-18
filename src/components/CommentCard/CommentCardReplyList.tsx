093a622e5d33172510f9537d604284e5ba378002
paopapaopao <paolo.casugay@gmail.com>
2025-01-18 15:19:13 +0800

    Refactor

--- src/components/CommentCard/CommentCardReplyList.tsx
+++ src/components/CommentCard/CommentCardReplyList.tsx
@@ -4,13 +4,13 @@ import clsx from 'clsx';
 import { type ReactNode } from 'react';
 
 import { REPLIES_FETCH_COUNT } from '@/lib/constants';
+import { useReadReplies } from '@/lib/hooks';
 import { type CommentWithRelationsAndRelationCountsAndUserReaction } from '@/lib/types';
 
 import { CommentCardSkeleton } from '../CommentCardSkeleton';
 
 import CommentCard from './CommentCard';
 import useCommentCard from './useCommentCard';
-import { useReadReplies } from '@/lib/hooks';
 
 const CommentCardReplyList = (): ReactNode => {
   const { comment } = useCommentCard();
