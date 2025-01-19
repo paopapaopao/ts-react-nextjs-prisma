99d4db807061e93214abeaf34602bca4bdcbc439
paopapaopao <paolo.casugay@gmail.com>
2025-01-19 15:24:32 +0800

    Update SearchField

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,50 +1,28 @@
 'use client';
 
-import {
-  type ReadonlyURLSearchParams,
-  useRouter,
-  useSearchParams,
-} from 'next/navigation';
-import { type ChangeEvent, type KeyboardEvent, type ReactNode } from 'react';
+import Form from 'next/form';
+import { type ReactNode } from 'react';
 import { MdSearch } from 'react-icons/md';
 import { Input } from '@nextui-org/react';
 
 const SearchField = (): ReactNode => {
-  const searchParams: ReadonlyURLSearchParams = useSearchParams();
-  const params: URLSearchParams = new URLSearchParams(searchParams);
-  const { push } = useRouter();
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.value !== '') {
-      params.set('query', event.target.value);
-    } else {
-      params.delete('query');
-    }
-  };
-
-  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>): void => {
-    if (event.key === 'Enter') {
-      push(`/search?${params.toString()}`);
-    }
-  };
-
   return (
-    <Input
-      defaultValue={searchParams.get('query')?.toString()}
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-      startContent={
-        <MdSearch
-          className='text-black'
-          size={32}
-        />
-      }
-      placeholder='Search posts'
-      size='lg'
-      radius='sm'
-      variant='faded'
-      className='min-w-[344px] w-full max-w-screen-lg'
-    />
+    <Form action='/search'>
+      <Input
+        name='query'
+        startContent={
+          <MdSearch
+            className='text-black'
+            size={32}
+          />
+        }
+        placeholder='Search posts'
+        size='lg'
+        radius='sm'
+        variant='faded'
+        className='min-w-[344px] w-full max-w-screen-lg'
+      />
+    </Form>
   );
 };
 
