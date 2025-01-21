21aebb64c7471cb30d10b5974ca9a2a602fb0d96
paopapaopao <paolo.casugay@gmail.com>
2025-01-21 15:34:57 +0800

    Delete CommentMutationStore type

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -6,8 +6,6 @@ export { type default as PostSchema } from './PostSchema';
 export { type default as ReactionSchema } from './ReactionSchema';
 export { type default as ViewSchema } from './ViewSchema';
 
-export { type default as CommentMutationStore } from './CommentMutationStore';
-
 export { type default as DummyJSONComment } from './DummyJSONComment';
 export { type default as DummyJSONPost } from './DummyJSONPost';
 export { type default as DummyJSONUser } from './DummyJSONUser';
