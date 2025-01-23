52595cb74e2191b6e60de75916d3708194701087
paopapaopao <paolo.casugay@gmail.com>
2025-01-23 14:18:46 +0800

    Create PUT and DELETE handlers for /api/reactions/[id] route

---
+++ src/app/api/reactions/[id]/route.ts
@@ -0,0 +1,94 @@
+import { revalidatePath } from 'next/cache';
+import { type NextRequest, NextResponse } from 'next/server';
+import { type SafeParseReturnType } from 'zod';
+import { type Reaction } from '@prisma/client';
+
+import { prisma } from '@/lib/db';
+import { reactionSchema } from '@/lib/schemas';
+import { type ReactionSchema } from '@/lib/types';
+
+type Params = {
+  params: Promise<{ id: string }>;
+};
+
+type Return = {
+  data: { reaction: Reaction | null } | null;
+  errors: { [key: string]: string[] } | unknown | null;
+  success: boolean;
+};
+
+const PUT = async (
+  request: NextRequest,
+  { params }: Params
+): Promise<NextResponse<Return>> => {
+  const payload: ReactionSchema = await request.json();
+
+  const parsedPayload: SafeParseReturnType<ReactionSchema, ReactionSchema> =
+    reactionSchema.safeParse(payload);
+
+  if (!parsedPayload.success) {
+    return NextResponse.json({
+      data: null,
+      errors: parsedPayload.error?.flatten().fieldErrors,
+      success: false,
+    });
+  }
+
+  try {
+    const id: string = (await params).id;
+
+    const response: Reaction | null = await prisma.reaction.update({
+      where: { id },
+      data: parsedPayload.data,
+    });
+
+    revalidatePath('/');
+    revalidatePath(`/posts/${response?.postId}`);
+
+    return NextResponse.json({
+      data: { reaction: response },
+      errors: null,
+      success: true,
+    });
+  } catch (error: unknown) {
+    console.error(error);
+
+    return NextResponse.json({
+      data: null,
+      errors: error,
+      success: false,
+    });
+  }
+};
+
+const DELETE = async (
+  _: NextRequest,
+  { params }: Params
+): Promise<NextResponse<Return>> => {
+  try {
+    const id: string = (await params).id;
+
+    const response: Reaction | null = await prisma.reaction.delete({
+      where: { id },
+    });
+
+    revalidatePath('/');
+    revalidatePath(`/posts/${response?.postId}`);
+
+    return NextResponse.json({
+      data: { reaction: response },
+      errors: null,
+      success: true,
+    });
+  } catch (error: unknown) {
+    console.error(error);
+
+    return NextResponse.json({
+      data: null,
+      errors: error,
+      success: false,
+    });
+  }
+};
+
+export { DELETE, PUT };
