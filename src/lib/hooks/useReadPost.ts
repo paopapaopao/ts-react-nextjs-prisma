481bee6e8af987e9dc5a64e7159b2ad97792ee8d
paopapaopao <paolo.casugay@gmail.com>
2025-01-27 10:46:38 +0800

    Refactor

--- src/lib/hooks/useReadPost.ts
+++ src/lib/hooks/useReadPost.ts
@@ -1,12 +1,15 @@
 'use client';
 
-import { type Post } from '@prisma/client';
+// import { type Post } from '@prisma/client';
 import { type UseQueryResult, useQuery } from '@tanstack/react-query';
 
 import { QueryKey } from '../enums';
+import { type PostWithRelationsAndRelationCountsAndUserReaction } from '../types';
 
 type TPost = {
-  data: { post: Post | null } | null;
+  data: {
+    post: PostWithRelationsAndRelationCountsAndUserReaction | null;
+  } | null;
   errors: { [key: string]: string[] } | unknown | null;
   success: boolean;
 };
