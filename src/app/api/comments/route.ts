671504676452a3011a9cfca221a1aae278c22dfb
paopapaopao <paolo.casugay@gmail.com>
2025-02-03 16:50:30 +0800

    Update POST handler for /api/comments route

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,27 +1,55 @@
 import { revalidatePath } from 'next/cache';
 import { type NextRequest, NextResponse } from 'next/server';
 import { type SafeParseReturnType } from 'zod';
+import { auth } from '@clerk/nextjs/server';
 import { type Comment } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 import { prisma } from '@/lib/db';
 import { commentSchema } from '@/lib/schemas';
 import type { CommentSchema, TComment } from '@/lib/types';
 
 const POST = async (request: NextRequest): Promise<NextResponse<TComment>> => {
-  const payload: CommentSchema = await request.json();
+  try {
+    const { userId } = await auth();
 
-  const parsedPayload: SafeParseReturnType<CommentSchema, CommentSchema> =
-    commentSchema.safeParse(payload);
+    if (userId === null) {
+      return NextResponse.json(
+        {
+          data: null,
+          errors: { auth: ['User unauthenticated/unauthorized'] },
+        },
+        { status: 401 }
+      );
+    }
+  } catch (error: unknown) {
+    console.error('User auth error:', error);
 
-  if (!parsedPayload.success) {
-    return NextResponse.json({
-      data: null,
-      errors: parsedPayload.error?.flatten().fieldErrors,
-      success: false,
-    });
+    return NextResponse.json(
+      {
+        data: null,
+        errors: { auth: ['User auth failed'] },
+      },
+      { status: 401 }
+    );
   }
 
   try {
+    const payload: CommentSchema = await request.json();
+
+    const parsedPayload: SafeParseReturnType<CommentSchema, CommentSchema> =
+      commentSchema.safeParse(payload);
+
+    if (!parsedPayload.success) {
+      return NextResponse.json(
+        {
+          data: null,
+          errors: parsedPayload.error?.flatten().fieldErrors,
+        },
+        { status: 400 }
+      );
+    }
+
     const response: Comment | null = await prisma.comment.create({
       data: parsedPayload.data,
     });
@@ -29,19 +57,35 @@ const POST = async (request: NextRequest): Promise<NextResponse<TComment>> => {
     revalidatePath('/');
     revalidatePath(`/posts/${response?.postId}`);
 
-    return NextResponse.json({
-      data: { comment: response },
-      errors: null,
-      success: true,
-    });
+    return NextResponse.json(
+      {
+        data: { comment: response },
+        errors: null,
+      },
+      { status: 200 }
+    );
   } catch (error: unknown) {
-    console.error(error);
+    if (error instanceof PrismaClientKnownRequestError) {
+      console.error('Comment create error:', error);
 
-    return NextResponse.json({
-      data: null,
-      errors: error,
-      success: false,
-    });
+      return NextResponse.json(
+        {
+          data: null,
+          errors: { database: ['Comment create failed'] },
+        },
+        { status: 500 }
+      );
+    }
+
+    console.error('Payload parse error:', error);
+
+    return NextResponse.json(
+      {
+        data: null,
+        errors: { server: ['Internal server error'] },
+      },
+      { status: 500 }
+    );
   }
 };
 
