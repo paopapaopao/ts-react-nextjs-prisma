551624c2a2cf31fe473b8c2fa1cfc7cdcc69b9c6
paopapaopao <paolo.casugay@gmail.com>
2025-02-04 16:02:02 +0800

    Update GET handler for /api/posts route

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,6 @@
 import { revalidatePath } from 'next/cache';
 import { type NextRequest, NextResponse } from 'next/server';
 import { type SafeParseReturnType } from 'zod';
-import { auth } from '@clerk/nextjs/server';
 import { type Post, Prisma } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
@@ -73,60 +72,79 @@ const POST = async (request: NextRequest): Promise<NextResponse<TPost>> => {
 };
 
 const GET = async (request: NextRequest): Promise<NextResponse<TPosts>> => {
-  const { searchParams } = new URL(request.url);
-  const cursor: number = Number(searchParams.get('cursor'));
+  const authUserResult = await authUser<TPosts>();
 
-  const { userId } = await auth();
+  if (authUserResult instanceof NextResponse) {
+    return authUserResult;
+  }
 
-  const posts = await prisma.post.findMany({
-    ...(cursor > 0 && {
-      cursor: { id: cursor },
-      skip: 1,
-    }),
-    include: {
-      user: true,
-      originalPost: {
-        include: { user: true },
-      },
-      _count: {
-        select: {
-          shares: true,
-          comments: {
-            where: { parentCommentId: null },
+  try {
+    const { searchParams } = new URL(request.url);
+    const cursor: number = Number(searchParams.get('cursor'));
+
+    const posts = await prisma.post.findMany({
+      ...(cursor > 0 && {
+        cursor: { id: cursor },
+        skip: 1,
+      }),
+      include: {
+        user: true,
+        originalPost: {
+          include: { user: true },
+        },
+        _count: {
+          select: {
+            shares: true,
+            comments: {
+              where: { parentCommentId: null },
+            },
+            reactions: true,
+            views: true,
           },
-          reactions: true,
-          views: true,
+        },
+        reactions: {
+          where: { clerkUserId: authUserResult.userId },
         },
       },
-      reactions: {
-        where: { clerkUserId: userId },
+      take: POSTS_FETCH_COUNT,
+      orderBy: { updatedAt: Prisma.SortOrder.desc },
+    });
+
+    const postsWithUserReaction = posts.map((post) => {
+      const { reactions, ...postWithoutReactions } = post;
+      const userReaction = reactions?.[0] ?? null;
+
+      return {
+        ...postWithoutReactions,
+        userReaction,
+      };
+    });
+
+    const hasMore: boolean = postsWithUserReaction.length > 0;
+
+    return NextResponse.json(
+      {
+        data: {
+          posts: postsWithUserReaction,
+          nextCursor: hasMore
+            ? postsWithUserReaction[postsWithUserReaction.length - 1].id
+            : null,
+        },
+        errors: null,
       },
-    },
-    take: POSTS_FETCH_COUNT,
-    orderBy: { updatedAt: Prisma.SortOrder.desc },
-  });
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    console.error('Post find many error:', error);
 
-  const postsWithUserReaction = posts.map((post) => {
-    const { reactions, ...postWithoutReactions } = post;
-    const userReaction = reactions?.[0] ?? null;
-
-    return {
-      ...postWithoutReactions,
-      userReaction,
-    };
-  });
-
-  const hasMore: boolean = postsWithUserReaction.length > 0;
-
-  return NextResponse.json({
-    data: {
-      posts: postsWithUserReaction,
-      nextCursor: hasMore
-        ? postsWithUserReaction[postsWithUserReaction.length - 1].id
-        : null,
-    },
-    errors: null,
-  });
+    return NextResponse.json(
+      {
+        data: null,
+        errors: { database: ['Post find many failed'] },
+      },
+      { status: 500 }
+    );
+  }
 };
 
 export { GET, POST };
