05231fbfa364d31d21003a155e7c7ce41397bbd6
paopapaopao <paolo.casugay@gmail.com>
2025-02-05 22:50:11 +0800

    Refactor schemas

--- src/lib/schemas/viewSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-const viewSchema = z.object({
-  userId: z.number().int().positive().finite(),
-  postId: z.number().int().positive().finite(),
-});
-
-export default viewSchema;
