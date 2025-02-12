159312d815fed6f65d478f7ceeefdf2e579ce04a
paopapaopao <paolo.casugay@gmail.com>
2025-02-12 17:39:05 +0800

    Create getCommentQueryKey utility function

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,6 +1,7 @@
 export { default as authenticateUser } from './authenticateUser';
 export { default as parsePayload } from './parsePayload';
 
+export { default as getCommentQueryKey } from './getCommentQueryKey';
 export { default as getPostQueryKey } from './getPostQueryKey';
 
 export { default as getName } from './getName';
