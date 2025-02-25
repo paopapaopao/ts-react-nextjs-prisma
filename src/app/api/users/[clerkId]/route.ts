430a8f87d92ad0a76e256ff036532466f8e21bd1
paopapaopao <paolo.casugay@gmail.com>
2025-02-25 10:45:38 +0800

    Refactor user api routes

--- src/app/api/users/[clerkId]/route.ts
+++ src/app/api/users/[clerkId]/route.ts
@@ -1,8 +1,8 @@
 import { type NextRequest, NextResponse } from 'next/server';
-import { type User } from '@prisma/client';
 
 import { prisma } from '@/lib/db';
 import type { UserQuery } from '@/lib/types';
+import { authenticateUser } from '@/lib/utils';
 
 type Params = {
   params: Promise<{ clerkId: string }>;
@@ -12,16 +12,47 @@ const GET = async (
   _: NextRequest,
   { params }: Params
 ): Promise<NextResponse<UserQuery>> => {
-  const clerkId: string = (await params).clerkId;
+  const authenticateUserResult = await authenticateUser<UserQuery>();
 
-  const response: User | null = await prisma.user.findUnique({
-    where: { clerkId },
-  });
+  if (authenticateUserResult instanceof NextResponse) {
+    return authenticateUserResult;
+  }
 
-  return NextResponse.json({
-    data: { user: response },
-    errors: null,
-  });
+  try {
+    const clerkId = (await params).clerkId;
+
+    const response = await prisma.user.findUnique({
+      where: { clerkId },
+    });
+
+    if (response === null) {
+      return NextResponse.json(
+        {
+          data: { user: null },
+          errors: null,
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        data: { user: response },
+        errors: null,
+      },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    console.error('User find unique error:', error);
+
+    return NextResponse.json(
+      {
+        data: null,
+        errors: { database: ['User find unique failed'] },
+      },
+      { status: 500 }
+    );
+  }
 };
 
 export { GET };
