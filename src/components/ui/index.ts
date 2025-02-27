61a71b4455aaa586a819dcf85869ca7367668eb4
paopapaopao <paolo.casugay@gmail.com>
2025-02-27 11:12:27 +0800

    Refactor

---
+++ src/components/ui/index.ts
@@ -0,0 +1,19 @@
+export { Input } from './input';
+export {
+  navigationMenuTriggerStyle,
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+  NavigationMenuIndicator,
+  NavigationMenuViewport,
+} from './navigation-menu';
+export {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverAnchor,
+} from './popover';
+export { Skeleton } from './skeleton';
