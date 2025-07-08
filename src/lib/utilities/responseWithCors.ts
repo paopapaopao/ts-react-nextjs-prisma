fb8d4b9a7046d31ac57cce68984131156843ce79
paopapaopao <paolo.casugay@gmail.com>
2025-07-08 22:36:09 +0800

    Refactor

--- src/lib/utilities/responseWithCors.ts
+++ src/lib/utilities/responseWithCors.ts
@@ -1,8 +1,8 @@
 import { type NextResponse } from 'next/server';
 
-export const responseWithCors = <TData>(
-  response: NextResponse<TData>
-): NextResponse<TData> => {
+export const responseWithCors = <TResponse>(
+  response: NextResponse<TResponse>
+): NextResponse<TResponse> => {
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
   response.headers.set('Content-Type', 'application/json');
