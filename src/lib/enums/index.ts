f537f70563df13d659fd979134c0308574326c73
paopapaopao <paolo.casugay@gmail.com>
2025-07-08 17:24:20 +0800

    Create HttpMethods enum

--- src/lib/enums/index.ts
+++ src/lib/enums/index.ts
@@ -1,3 +1,11 @@
+export enum HttpMethods {
+  DELETE = 'DELETE',
+  GET = 'GET',
+  OPTIONS = 'OPTIONS',
+  POST = 'POST',
+  PUT = 'PUT',
+}
+
 export enum QueryKey {
   COMMENTS = 'COMMENTS',
   POSTS = 'POSTS',
