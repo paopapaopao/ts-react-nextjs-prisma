3a6b919e7680126aa64dea38b1e33de49c433cb8
paopapaopao <paolo.casugay@gmail.com>
2025-07-09 21:47:58 +0800

    Delete readCommentWithRelationsAndRelationCountsAndUserReaction and readPostWithRelationsAndRelationCountsAndUserReaction actions

--- src/lib/types/action-returns/index.ts
+++ src/lib/types/action-returns/index.ts
@@ -1,9 +1,73 @@
 import { Prisma } from '@prisma/client';
 
-import {
-  readCommentWithRelationsAndRelationCountsAndUserReaction,
-  readPostWithRelationsAndRelationCountsAndUserReaction,
-} from '@/lib/actions';
+import { prisma } from '@/lib/db';
+
+const readCommentWithRelationsAndRelationCountsAndUserReaction = async (
+  id: number,
+  clerkUserId: string | null
+) => {
+  const response = await prisma.comment.findUnique({
+    where: { id },
+    include: {
+      user: true,
+      _count: {
+        select: {
+          replies: true,
+          reactions: true,
+        },
+      },
+      reactions: {
+        where: { clerkUserId },
+      },
+    },
+  });
+
+  if (response === null) {
+    return null;
+  }
+
+  const { reactions, ...responseWithoutReactions } = response;
+  const userReaction = reactions[0] ?? null;
+
+  return { ...responseWithoutReactions, userReaction };
+};
+
+const readPostWithRelationsAndRelationCountsAndUserReaction = async (
+  id: number,
+  clerkUserId: string | null
+) => {
+  const response = await prisma.post.findUnique({
+    where: { id },
+    include: {
+      user: true,
+      originalPost: {
+        include: { user: true },
+      },
+      _count: {
+        select: {
+          shares: true,
+          comments: {
+            where: { parentCommentId: null },
+          },
+          reactions: true,
+          views: true,
+        },
+      },
+      reactions: {
+        where: { clerkUserId },
+      },
+    },
+  });
+
+  if (response === null) {
+    return null;
+  }
+
+  const { reactions, ...responseWithoutReactions } = response;
+  const userReaction = reactions[0] ?? null;
+
+  return { ...responseWithoutReactions, userReaction };
+};
 
 export type CommentWithRelationsAndRelationCountsAndUserReaction =
   Prisma.PromiseReturnType<
