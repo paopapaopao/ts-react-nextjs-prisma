ce45e9ae35c5d39cff4435775c6c004611b8be63
paopapaopao <paolo.casugay@gmail.com>
2025-07-09 23:56:16 +0800

    Delete db

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,1 +1,12 @@
-export { prisma } from './db';
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = (): PrismaClient => new PrismaClient();
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const prisma: PrismaClient =
+  globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
