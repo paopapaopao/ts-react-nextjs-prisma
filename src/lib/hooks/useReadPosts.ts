a03e0912b027a97b4700832e769794e8ca839732
paopapaopao <paolo.casugay@gmail.com>
2025-07-26 18:14:36 +0800

    Update useReadPosts

--- src/lib/hooks/useReadPosts.ts
+++ src/lib/hooks/useReadPosts.ts
@@ -10,6 +10,7 @@ import { QueryKey } from '../enumerations';
 import type { PageParam, PostInfiniteQuery } from '../types';
 
 export const useReadPosts = (
+  userId?: number,
   clerkUserId?: string | null,
   query?: string | null
 ): UseInfiniteQueryResult<
@@ -17,22 +18,24 @@ export const useReadPosts = (
   Error
 > => {
   return useInfiniteQuery({
-    queryKey: [QueryKey.POSTS, { clerkUserId, query }],
+    queryKey: [QueryKey.POSTS, { userId, clerkUserId, query }],
     queryFn: async ({ pageParam }: PageParam): Promise<PostInfiniteQuery> => {
       const params = new URLSearchParams();
+      params.append('cursor', String(pageParam));
+
+      if (userId !== undefined) {
+        params.append('userId', String(userId));
+      }
 
       if (clerkUserId !== undefined && clerkUserId !== null) {
         params.append('clerkUserId', clerkUserId);
       }
 
-      if (query?.trim()) {
+      if (query !== undefined && query !== null && query.trim() !== '') {
         params.append('query', query.trim());
       }
 
-      const response = await fetch(
-        `/api/posts?clerkUserId=${clerkUserId}&query=${query}&cursor=${pageParam}`
-      );
-
+      const response = await fetch(`/api/posts?${params.toString()}`);
       const result: PostInfiniteQuery = await response.json();
 
       if (!response.ok && result.errors !== null) {
